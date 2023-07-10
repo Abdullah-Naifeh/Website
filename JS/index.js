122289383a5a22421d1408dc62b1393b7aa54ab9
Abdullah-Naifeh <abdna59@gmail.com>
2023-07-10 04:19:14 +0300

    Add About page and moified main page content

---
+++ JS/index.js
@@ -0,0 +1,16 @@
+const mail = document.getElementsByClassName('mail')[0]
+const linkedin = document.getElementsByClassName('linkedIn')[0]
+const github = document.getElementsByClassName('github')[0]
+
+
+mail.addEventListener('click', function () {
+    window.open('mailto:abdna59@gmail.com')
+});
+linkedin.addEventListener('click', function () {
+    window.open('https://tr.linkedin.com/in/abdullah-naifeh-9a82a1149')
+});
+github.addEventListener('click', function () {
+    window.open('https://github.com/Abdullah-Naifeh')
+});
+
+
