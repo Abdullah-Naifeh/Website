f51fb30cec085f950d0b82604f933b39b29199bc
Abdullah-Naifeh <abdna59@gmail.com>
2023-07-15 06:20:27 +0300

    Create hamburger menu for mobile divices

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,9 @@
 const mail = document.getElementsByClassName('mail')[0]
 const linkedin = document.getElementsByClassName('linkedIn')[0]
 const github = document.getElementsByClassName('github')[0]
+const hamburger = document.querySelector('.hamburger')
+const navMenu = document.querySelector('.navbar-nav')
+const navLinks = document.querySelectorAll('.nav-link')
 
 
 mail.addEventListener('click', function () {
@@ -12,5 +15,15 @@ linkedin.addEventListener('click', function () {
 github.addEventListener('click', function () {
     window.open('https://github.com/Abdullah-Naifeh')
 });
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active')
+    navMenu.classList.toggle('active')
+})
+
+navLinks.forEach( n => n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+} ))
 
 
